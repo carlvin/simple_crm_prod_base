a8c734b541bd9d7511cb8b5e14972ac77ef6c431
carlvin <calvinokumu@gmail.com>
2023-06-26 15:14:40 +0300

    small changes

--- client_relationship_manager/views.py
+++ client_relationship_manager/views.py
@@ -17,6 +17,7 @@ from client_relationship_manager.models import Client, Device
 # Create your views here.
 class HomeView(ListView):
     template_name = "index.html"
+    context_object_name = "clients"
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         return super().get_context_data(**kwargs)
