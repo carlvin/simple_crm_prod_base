e2c1dd91034f251a65860797421233025d3029fe
carlvin <calvinokumu@gmail.com>
2023-06-26 18:10:31 +0300

    small changes

--- config/settings/base.py
+++ config/settings/base.py
@@ -4,7 +4,14 @@ from pathlib import Path
 my_variable =os.environ.get('DJANGO_SETTINGS_MODULE')
 print(f"By:Carlvin:: This is the Module setting running:{ my_variable }")
 
-#print(os.environ.get('SECRET_KEY'))
+# Get the value of an environmental variable
+secret_key = os.environ.get('SECRET_KEY')
+
+# Check if the environmental variable exists and retrieve its value
+if secret_key:
+    print(f"The SECRET_KEY is: {secret_key}")
+else:
+    print("SECRET_KEY is not set.")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
