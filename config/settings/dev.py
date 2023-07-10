26ec144e9292350fc749fdf38bca1c96c680ce4e
carlvin <calvinokumu@gmail.com>
2023-07-11 01:11:04 +0300

    small changes

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,1 +1,12 @@
 from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASS'),
+        'HOST': '127.0.0.1',
+        'PORT':"",
+    }
+}
